97b0f2a8842dd4b2163d79ea430ebca26341a282
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-06-19 19:41:01 +0530

    upload

---
+++ 19-06-2025/square-pattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SquarePattern {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the square: ");
+        int size = scanner.nextInt();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("* "); 
+            }
+            System.out.println(); 
+        }
+        scanner.close();
+    }
+}

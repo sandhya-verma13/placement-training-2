a6ce9eba4822e0f5d9d73dfc92747d272171cae0
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-06-20 19:10:12 +0530

    Create prime

---
+++ 20-06-2025/prime.java
@@ -0,0 +1,21 @@
+public class PrimeChecker {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int number = 17;
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}

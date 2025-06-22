16beb733d415e51a3f55bd591d4a374cd598109b
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-06-22 18:38:09 +0530

    Create vowel_or_constant

---
+++ 22-06-2025/vowel_or_constant.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Geeks {
+    public static void main(String args[]) {
+        char ch = 'b';
+         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o'
+            || ch == 'u') {
+            System.out.println(ch+" is a vowel");
+        }else {
+            System.out.println(ch+" is a consonant");
+      }
+    }
+}
